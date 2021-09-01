e913b9eec1b3f67c75db8ed1c854d764730c3832
Ajmal <Sohaib_Ajmal@Dellteam.com>
2021-09-01 12:37:55 +0500

    architecture update

---
+++ test_main.py
@@ -0,0 +1,16 @@
+from features.numa import *
+
+
+def setup():
+    //Read Settings Files
+    //Read Baremetsl Nodes List
+    //Setup Environment
+    
+    //return settings
+
+def(tear_down):
+    // Tear Down Data
+
+@pytets.mark.numa
+def test_addition():
+    assert numa_add(2,6)= 8
