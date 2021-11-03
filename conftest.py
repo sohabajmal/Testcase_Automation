99571dba42f3c9e3924a0b97d5f878df03576155
sohabajmal <34816448+sohabajmal@users.noreply.github.com>
2021-11-03 12:41:39 +0500

    Delete conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-'''
-def pytest_configure(config):
-    if not config.option.resultlog:
-        timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d_%H-%M-%S')
-        config.option.resultlog = 'log.' + timestamp
-'''
